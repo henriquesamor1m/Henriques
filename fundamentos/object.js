bcc6bee5e02d79ccc2b0745a299a34a474c64dfa
henriquesamor1m <henriquesnetto@hotmail.com>
2023-09-14 20:35:38 -0300

    first commit

---
+++ fundamentos/object.js
@@ -0,0 +1,15 @@
+const prod1 = {}
+prod1.nome = 'celular ultra mega'
+prod1.preco= 4998.90
+prod1['desconto legal']=0.40  //evitar atributos com espaços
+
+console.log(prod1)
+
+const prod2= {
+    nome: 'camisa polo',
+    preco: 79.90,
+}
+
+'{ "nome": "camisa polo", "preco": 79.90}'
+
+console.log(prod2)
